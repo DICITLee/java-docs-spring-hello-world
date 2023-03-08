622e3ad34b816f81a60cc2781d67ac0effc8ade3
DICITLee <127267883+DICITLee@users.noreply.github.com>
2023-03-08 16:37:32 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,13 +16,13 @@ private static String endpointUri = "https://supplierinfor.documents.azure.com:4
 private static String primaryKey = "rH5gW2kw0Qwlyj0Lp10gD9izLYlRymef9deFm8Ifd2latO1NHSpQLAyoD8VKisAgEk81nAvBv3C9ACDbRhVn2g==";
 	
 /** Azure Cosmos DB client instance. */
-/**private static CosmosAsyncClient client;
+/**private static CosmosAsyncClient client;*/
 
 /** Azure Cosmos DB database instance. */
-/**private static CosmosAsyncDatabase database;
+/**private static CosmosAsyncDatabase database;*/
 
 /** Azure Cosmos DB container instance. */
-/**private static CosmosAsyncContainer container;
+/**private static CosmosAsyncContainer container;*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
